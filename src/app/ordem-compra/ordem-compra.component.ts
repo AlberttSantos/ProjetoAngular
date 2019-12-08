a19e79c30dd7b3fb03f9071a290b01ffc45fcc22
Albertt <alberttaurelio@gmail.com>
2019-12-08 16:01:21 -0200

    Finalizando template forms
    Finalização das aulas de template forms.

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -1,21 +1,31 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { OrdemCompraService } from '../services/ordem-compra.service';
+import { Pedido } from '../model/pedido.model';
 
 @Component({
   selector: 'app-ordem-compra',
   templateUrl: './ordem-compra.component.html',
   styleUrls: ['./ordem-compra.component.css'],
-  providers: []
+  providers: [OrdemCompraService]
 })
 export class OrdemCompraComponent implements OnInit {
   @ViewChild('formulario', { static: false }) public form: NgForm
+  public idPedido: number;
 
-  constructor() { }
+  constructor(private ordemCompraService: OrdemCompraService) { }
 
   ngOnInit() {
   }
 
   confirmar() {
-    console.log(this.form)
+    let pedido = new Pedido(
+      this.form.value.endereco,
+      this.form.value.numero,
+      this.form.value.complemento,
+      this.form.value.formaPagamento);
+
+    this.ordemCompraService.efetivarCompra(pedido)
+      .subscribe((id: number) => this.idPedido = id);
   }
 }
